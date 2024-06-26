c62141ab10924d48f59a9e80d34b8abd30fc112a
Damien SIX <damien@robotsix.net>
2024-06-26 12:40:12 +0200

    ongoing work

--- src/modes_runner.cpp
+++ src/modes_runner.cpp
@@ -2,15 +2,16 @@
 #include <ros2_px4_interface/modes/spin.hpp>
 #include <ros2_uav_parameters/parameter_client.hpp>
 
+using uav_cpp::parameters::ParameterMap;
 using ArmSpinMode = px4_ros2::NodeWithModeExecutor<ros2_uav::mode::ExecutorArmSpin,
     ros2_uav::mode::Spin>;
 
 int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
-  std::vector<std::string> required_parameters = {"spin_thrust"};
+  std::vector<std::string> required_parameters{"spin_thrust"};
   auto parameter_client_ = std::make_shared<ros2_uav::parameters::ParameterClient>("spin_parameters", required_parameters);
-  auto parameters = parameter_client_->
+  auto parameters = std::make_shared<ParameterMap>(parameter_client_->getParameters());
   rclcpp::spin(std::make_shared<ArmSpinMode>(parameters));
   rclcpp::shutdown();
   return 0;
